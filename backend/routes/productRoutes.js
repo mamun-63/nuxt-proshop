4157dd00dc7d01ebdbdda6f312633b2310a53275
mamun-63 <mamun.cse63@gmail.com>
2021-03-20 21:24:14 +0600

    7.1 clean up routes by controllers

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,37 +1,8 @@
 import express from "express"
 const router = express.Router()
-import Product from "../models/productModel.js"
-import asyncHandler from "express-async-handler"
+import { getProducts, getProductById } from '../controllers/productController.js'
 
-// async route
-// to handle error instead of writting try catch everytime, using express-async-handler npm package
-
-// @desc    Fetch all products
-// @route   GET /api/products
-// @access  public
-router.get(
-  "/",
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({}) // will return everything
-    res.json(products)
-  })
-)
-
-// @desc    Fetch single products
-// @route   GET /api/products/:id
-// @access  public
-router.get(
-  "/:id",
-  asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id)
-    if (product) {
-      res.json(product)
-    } else {
-      // As we habe made custom error handler, so we can through error now, by default it 500 if status not set
-      res.status(404)
-      throw new Error("Product not found")
-    }
-  })
-)
+router.route("/").get(getProducts)
+router.route("/:id").get(getProductById)
 
 export default router
